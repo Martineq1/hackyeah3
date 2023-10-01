6f698b4f18bbe5c4c89849e5ab85cfcbc8681919
Marcin Wluka <Marwluka@gmail.com>
2023-10-01 11:55:36 +0200

    usuniecie jednego pliku

--- testowaniemodelu.py
+++ testowaniemodelu.py
@@ -3,7 +3,7 @@ import requests
 import json
 import os
 
-API_KEY = "***" 
+API_KEY = "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" 
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 FolderPath2 = "F:\Git\hackyeah3\plikitreningowe\\trening1.json"
 datatraining = ""
@@ -14,7 +14,8 @@ completion = openai.ChatCompletion.create(
   model="ft:gpt-3.5-turbo-0613:personal:chatner-bot:84m0uHhU",
   messages=[
     {"role": "system", "content": "Ten chatbot nazwany Munkiem pomaga mlodym osobom decydowac o wybraniu odpowiedniego zawodu "},
-    {"role": "user", "content": """"Kim na podstawie tych danych pokazujacych cechy charakteru i umiejetności Cierpliwosc: 5,Umiejetnosci Komunikacyjne:   4,Ciagle Uczenie sie: 2,Kreatywnosc: 4,Myslenie Analityczne: 3  ,Dostosowawczosc:  4,Zarzadzanie Czasem: 4,Wspolpraca:  3,Odpornosc: 4,Umiejetnosci Organizacyjne : 5,Ciekawosc: 3,Swiadomosc Etyczna:: 3,Dokumentacja: 4  najlepiej aby dana osoba zostala"""}
+    {"role": "user", "content": """"Kim na podstawie tych danych pokazujacych cechy charakteru i umiejetności Cierpliwosc: 5,Umiejetnosci Komunikacyjne:   
+     4,Ciagle Uczenie sie: 2,Kreatywnosc: 4,Myslenie Analityczne: 3  ,Dostosowawczosc:  4,Zarzadzanie Czasem: 4,Wspolpraca:  3,Odpornosc: 4,Umiejetnosci Organizacyjne : 5,Ciekawosc: 3,Swiadomosc Etyczna:: 3,Dokumentacja: 4  najlepiej aby dana osoba zostala"""}
      ]
 )             
 print(completion.choices[0].message)
@@ -24,7 +25,8 @@ completion = openai.ChatCompletion.create(
   model="ft:gpt-3.5-turbo-0613:personal:chatner-bot:84m0uHhU",
   messages=[
     {"role": "system", "content": "Ten chatbot nazwany Munkiem pomaga mlodym osobom decydowac o wybraniu odpowiedniego zawodu "},
-    {"role": "user", "content": """"Kim na podstawie tych danych pokazujacych cechy charakteru i umiejetności Cierpliwosc: 2,Umiejetnosci Komunikacyjne:   4,Ciagle Uczenie sie: 4,Kreatywnosc: 4,Myslenie Analityczne: 4  ,Dostosowawczosc:  3,Zarzadzanie Czasem: 2,Wspolpraca:  3,Odpornosc: 3,Umiejetnosci Organizacyjne : 3,Ciekawosc: 5,Swiadomosc Etyczna:: 3,Dokumentacja: 3  najlepiej aby dana osoba zostala"""}
+    {"role": "user", "content": """"Kim na podstawie tych danych pokazujacych cechy charakteru i umiejetności Cierpliwosc: 2,Umiejetnosci Komunikacyjne:   
+     4,Ciagle Uczenie sie: 4,Kreatywnosc: 4,Myslenie Analityczne: 4  ,Dostosowawczosc:  3,Zarzadzanie Czasem: 2,Wspolpraca:  3,Odpornosc: 3,Umiejetnosci Organizacyjne : 3,Ciekawosc: 5,Swiadomosc Etyczna:: 3,Dokumentacja: 3  najlepiej aby dana osoba zostala"""}
      ]
 )             
 print(completion.choices[0].message)
